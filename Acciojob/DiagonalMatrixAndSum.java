d0aa10eea2ff2942addd67b3ba1fa3575e702f90
Bhupathi2.0 <92458357+Bhupathi20@users.noreply.github.com>
2022-08-17 13:17:39 +0530

    Add files via upload

---
+++ Acciojob/DiagonalMatrixAndSum.java
@@ -0,0 +1,46 @@
+package Acciojob;
+
+import java.util.Scanner;
+
+public class DiagonalMatrixAndSum {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] arr= new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+ 
+                // Condition for principal diagonal
+                if (i == j) {
+                    count++;
+                }
+            }
+        }
+
+        int sum = 0;
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            if (i == j)
+               sum += arr[i][j];
+            }
+        }
+        if(count>n){
+            System.out.println("YES");
+        }else {
+            System.out.println("YES");
+        }
+
+
+        System.out.println(sum);
+    }
+}
